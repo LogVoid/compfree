5e58428f9486e17ec36d395a28e3be97b92b3460
logvoid <logvoid@proton.me>
2024-09-04 15:34:23 +0300

    feat: create get_products route

--- backend/routes.py
+++ backend/routes.py
@@ -1,6 +1,21 @@
 from flask import request, jsonify
 from config import app, db
+from models import Product
 
-@app.route('/hello', methods=['GET'])
-def hello():
-    return jsonify(message="Hello World!"), 201
+@app.route('/api/v1/products', methods=['GET'])
+def get_products():
+    try:
+        prods = Product.query.all()
+        prodl = []
+        for prod in prods:
+            prodl.append({
+                "id": prod.id,
+                "name": prod.name,
+                "description": prod.description,
+                "img": prod.img,
+                "price": prod.price
+            })
+        return jsonify(message="Products fetched successfully.", products=prodl), 201
+    except Exception as e:
+        return jsonify(message=f"Failed to fetch products: {e}"), 404
+
