d0716c682e3dc5109363ff9298d1364f44776ec5
logvoid <logvoid@proton.me>
2024-09-04 14:04:38 +0300

    feat: create Product model

---
+++ backend/models.py
@@ -0,0 +1,8 @@
+from config import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    img = db.Column(db.String(255), nullable=True)
+
