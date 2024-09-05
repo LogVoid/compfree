8de1d03c835c1c7b9ac0c7b028c5a9b8426c5566
logvoid <logvoid@proton.me>
2024-09-05 16:00:44 +0300

    feat: create get_product route

--- backend/routes.py
+++ backend/routes.py
@@ -15,7 +15,24 @@ def get_products():
                 "img": prod.img,
                 "price": prod.price
             })
-        return jsonify(message="Products fetched successfully.", products=prodl), 201
+        return jsonify(message="Products fetched successfully.", products=prodl), 200
     except Exception as e:
         return jsonify(message=f"Failed to fetch products: {e}"), 404
+
+@app.route('/api/v1/products/<int:pid>', methods=['GET'])
+def get_product(pid):
+    try:
+        product = Product.query.get(pid)
+        if product is None:
+            return jsonify(message="Product not found."), 404
+        
+        return jsonify({
+            "id": product.id,
+            "name": product.name,
+            "description": product.description,
+            "img": product.img,
+            "price": product.price
+        }), 200
+    except Exception as e:
+        return jsonify(message=f"Failed to fetch product: {e}"), 500
 
