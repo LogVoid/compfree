ed7940494b94fac0feae997579896358a0aafd77
logvoid <logvoid@proton.me>
2024-09-09 12:34:24 +0300

    feat: add images to Product component

--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { ProductItem } from '../interfaces/ProductItem';
 
 const Product: React.FC<ProductItem> = ({id, name, description, img, price}) => {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const imgUrl = `${backendUrl}/api/v1/products/${id}/image`;
+
   return (
     <div className="border rounded-lg p-4 shadow-md">
-      <h2 className="text-xl font-bold">
-        {name}
-      </h2>
-      <p className="text-gray-700">
-        Price: {price.toFixed(2)} €
-      </p>
+      <img src={imgUrl} alt="Logo"/>
+      <div>
+        <h2 className="text-xl font-bold">
+          {name}
+        </h2>
+        <p className="text-gray-700">
+          Price: {price.toFixed(2)} €
+        </p>
+      </div>
     </div>
   );
 };
